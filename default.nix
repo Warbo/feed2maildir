0e452fad641119b58390813288687ffc5401463f
Chris Warburton <chriswarbo@gmail.com>
2023-09-30 17:02:40 +0100

    Python 2 -> 3

--- default.nix
+++ default.nix
@@ -1,16 +1,18 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-with pkgs;
-
-pythonPackages.buildPythonPackage {
-  name = "feed2maildirsimple";
-  version = "0.4.0";
-
-  src = ./.;
-
-  propagatedBuildInputs = [
-    pythonPackages.python
-    pythonPackages.dateutil
-    pythonPackages.feedparser
-  ];
-}
+{ nixpkgs ? import (fetchTarball {
+  name = "nixpkgs2305";
+  url = "https://github.com/NixOS/nixpkgs/archive/23.05.tar.gz";
+  sha256 = "10wn0l08j9lgqcw8177nh2ljrnxdrpri7bp0g7nvrsn9rkawvlbf";
+}) {
+  config = { };
+  overlays = [ ];
+}, python3Packages ? nixpkgs.python3Packages }:
+with {
+  pkg = { buildPythonPackage, dateutil, feedparser, python }:
+    buildPythonPackage {
+      name = "feed2maildirsimple";
+      version = "0.4.0";
+      src = ./.;
+      propagatedBuildInputs = [ python dateutil feedparser ];
+    };
+};
+python3Packages.callPackage pkg { }
